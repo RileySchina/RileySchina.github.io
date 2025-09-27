055c5168c9d0d07f36fdd5025467706dea87ba05
Riley Schina <riley.schina@gmail.com>
2025-09-27 12:45:41 -0700

    Fixed the pie chart, added third option

--- vis.js
+++ vis.js
@@ -7,10 +7,11 @@ function showPieChart(){
     console.log("pie-chart on load");
 
     //the parts of the chart
-    let sliceA={size:250, color: 'blue'};
-    let sliceB={size:750, color: 'green'};
+    let sliceA={size:59.8, color: 'lightblue'}; //houses with dogs in millions
+    let sliceB={size:42.2, color: 'lightgreen'}; // houses with cats in millions
+    let sliceC={size:11.9, color: 'lightyellow'}; // houses with other pets in millions
 
-    const values = [sliceA.size, sliceB.size];
+    const values = [sliceA.size, sliceB.size, sliceC.size];
 
     const total = values.reduce((acc, val) => acc + val, 0);
 
@@ -44,7 +45,9 @@ function showPieChart(){
 
         //color the slices
 
-        ctx.fillStyle = index === 0 ? sliceA.color : sliceB.color;
+        // ctx.fillStyle = index === 0 ? sliceA.color : sliceB.color;
+        ctx.fillStyle = index === 0 ? sliceA.color : index === 1 ? sliceB.color : sliceC.color; 
+        //operator had to be switched from previous, yes, no to a 3 option of 0, 1, else. Add more index and number to add more options
         ctx.fill();
 
         startAngle += angle;
@@ -59,13 +62,19 @@ function showPieChart(){
     <div class="legend-item">
         <div class="legend-color" style="background-color:${sliceA.color}"</div>
 
-        <div class="legend-label"> Total Pets: $${sliceA.size} - ${((sliceA.size / total) * 100).toFixed(2)} %</div>
+        <div class="legend-label"> Houses with Dogs: ${sliceA.size} million ~ ${((sliceA.size / total) * 100).toFixed(2)} %</div>
     </div>
 
     <div class="legend-item">
         <div class="legend-color" style="background-color:${sliceB.color}"</div>
 
-        <div class="legend-label"> Total Pets: $${sliceB.size} - ${((sliceB.size / total) * 100).toFixed(2)} %</div>
+        <div class="legend-label"> Houses with Cats: ${sliceB.size} million ~ ${((sliceB.size / total) * 100).toFixed(2)} %</div>
+    </div>
+
+    <div class="legend-item">
+        <div class="legend-color" style="background-color:${sliceC.color}"</div>
+
+        <div class="legend-label"> Houses with other pets: ${sliceC.size} million ~ ${((sliceC.size / total) * 100).toFixed(2)} %</div>
     </div>
    ` ;
 
